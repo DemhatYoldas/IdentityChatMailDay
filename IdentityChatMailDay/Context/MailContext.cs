8bb807dc439f6b7f59ae4e62fd3232de9082ab57
DEMHAT <DEMHAT@DESKTOP-JQG1QG1>
2026-07-14 19:14:52 +0300

    feat: implement Identity user registration infrastructure

---
+++ IdentityChatMailDay/Context/MailContext.cs
@@ -0,0 +1,14 @@
+﻿using IdentityChatMailDay.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityChatMailDay.Context
+{
+    public class MailContext:IdentityDbContext<AppUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-JQG1QG1\\SQLEXPRESS;initial Catalog=EmailChatDb;integrated Security=true;trust server certificate=true;");
+        }
+    }
+}
