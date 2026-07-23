6785abec39295a8916af40a52a77918213c975ad
DEMHAT <DEMHAT@DESKTOP-JQG1QG1>
2026-07-23 15:53:59 +0300

    .

--- IdentityChatMailDay/Context/MailContext.cs
+++ IdentityChatMailDay/Context/MailContext.cs
@@ -4,11 +4,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IdentityChatMailDay.Context
 {
-    public class MailContext:IdentityDbContext<AppUser>
+    public class MailContext : IdentityDbContext<AppUser>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("server=DESKTOP-JQG1QG1\\SQLEXPRESS;initial Catalog=EmailChatDb;integrated Security=true;trust server certificate=true;");
         }
+
+        public DbSet<Message> Messages {get; set;}
+
+
     }
 }
---
+++ IdentityChatMailDay/Controllers/MessageController.cs
@@ -0,0 +1,53 @@
+﻿using IdentityChatMailDay.Context;
+using IdentityChatMailDay.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityChatMailDay.Controllers
+{
+    public class MessageController : Controller
+    {
+        private readonly MailContext _context;
+        private readonly UserManager<AppUser> _userManager;
+
+
+
+        public MessageController(MailContext context, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult Inbox()
+        {
+            return View();
+        }
+
+        public IActionResult Sendbox()
+        {
+            return View();
+        }
+
+
+        public IActionResult CreateMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateMessage(Message message)
+        {
+            message.SendDate = DateTime.Now;
+            message.IsRead = false;
+            _context.Messages.Add(message);
+            _context.SaveChanges();
+            return RedirectToAction("Sendbox");
+        }
+
+    }
+}
